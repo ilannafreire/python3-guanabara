ffacd71a41ca502306cac41db10eede8b907ce50
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-29 23:35:06 -0300

    feat: basico de python e anotações

---
+++ mundo1/basico.py
@@ -0,0 +1,22 @@
+
+print('Olá, Mundo!')
+
+print( 7 + 4)
+
+print('7' + '4')
+
+print('Ola' , 3)
+
+
+
+# mensagens têm delimitadores especiais : aspas simples ou duplas
+# em py, os devs usam aspas simples.
+# toda função tem que ter parenteses.
+# em py, numeros não estão entre aspas.
+# mensagens sao exibidas em tela
+# numeros - calculos - sem ''
+# no terceiro exemplo é exibido na tela 74, junta um ao outro.
+# o 74 não é interpretado como um numero, mas como a junção da mensagem 7 e 4.
+# as vezes o + ficará melhor que a , . Com a prática, o dev sentirá qual o melhor.
+# no ex do ola e 3, caso use o + dara erro de sintaxe. nesse caso, virgula.
+# escreva tudo em letras minusculas, se fora de aspas em py.
