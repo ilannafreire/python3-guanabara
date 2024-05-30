5d6469174545e88186849fdee39ffcb1a20b9a66
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-30 00:42:36 -0300

    feat: modo input e anotações

--- mundo1/variaveis1.py
+++ mundo1/variaveis1.py
@@ -1,6 +1,6 @@
-nome = 'ilanna'
-idade = 25
-peso = 69.5
+nome = input ('Qual é seu nome?')
+idade = input ('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
 
 print( nome, idade , peso)
 
@@ -17,10 +17,10 @@ print( nome, idade , peso)
 # no caso do py, toda variável é um objeto.
 # toda variavel pode receber valores. recebe simbolizado com sinal = 
 # = recebe
-# 
-# 
-# 
-# 
+# input - significa leia
+# o que o usuário digitar aparecerá dentro da variavel 
+# use modo interativo - testar algo 
+# use modo de programa - crie um script
 # 
 # 
 # 
