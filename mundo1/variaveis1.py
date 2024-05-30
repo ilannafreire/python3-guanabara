e699a1835bbfd116fd78d30046bfc4b455ed5eca
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-30 00:36:13 -0300

    feat: exemplo variaveis

---
+++ mundo1/variaveis1.py
@@ -0,0 +1,29 @@
+nome = 'ilanna'
+idade = 25
+peso = 69.5
+
+print( nome, idade , peso)
+
+
+
+
+
+
+
+
+
+
+
+# no caso do py, toda variável é um objeto.
+# toda variavel pode receber valores. recebe simbolizado com sinal = 
+# = recebe
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
